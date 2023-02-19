d35841cd0c376cb514e3b1d84bdd0b7a48a81804
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-02-19 22:27:05 +0300

    Add files via upload

---
+++ sheet.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE databasename;
+
+DROP DATABASE databasename;
+
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+);
+
+
+CREATE TABLE new_table_name AS
+    SELECT column1, column2,...
+    FROM existing_table_name
+    WHERE ....;
+
+
+DROP TABLE table_name;
+
+
+ALTER TABLE table_name
+ADD column_name datatype;
+
+
+ALTER TABLE table_name
+DROP COLUMN column_name;
+
+
+ALTER TABLE table_name
+MODIFY COLUMN column_name datatype;
+
+
+CREATE TABLE table_name (
+    column1 datatype constraint,
+    column2 datatype constraint,
+    column3 datatype constraint,
+    ....
+);
+
+/* contraint section siblings:
+NOT NULL
+UNIQUE
+PRIMARY KEY
+FOREIGN KEY
+CHECK
+DEFAULT
+CREATE INDEX
+*/
+
+--Check
+
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CHECK (Age>=18)
+);
+
+ALTER TABLE Persons
+ADD CHECK (Age>=18);
+
+ALTER TABLE Persons
+ADD CONSTRAINT CHK_PersonAge CHECK (Age>=18 AND City='Sandnes');
