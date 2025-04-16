e2e99de22c0da2d65487635218ecad2e1524da06
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2025-04-16 22:27:23 +0300

    Update and rename provinces (1).sql to provinces.sql

--- Turkish Provinces and Districts SQL/provinces.sql
+++ Turkish Provinces and Districts SQL/provinces.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Apr 16, 2025 at 08:47 PM
--- Server version: 11.4.5-MariaDB
--- PHP Version: 8.3.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `efotokopi_fotokopidb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `provinces`
---
-
 CREATE TABLE `provinces` (
   `id` int(11) NOT NULL,
   `province_name` varchar(100) NOT NULL,
